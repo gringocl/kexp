110f5f0c940e4c9debe76fed85225ad717c1da45
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-06-13 23:09:11 -0700

    Remove Commas

--- kexp.rb
+++ kexp.rb
@@ -10,8 +10,8 @@ db = mongo_client.db("kexp")
 coll = db.collection("playlist")
 
 dates = Date.new(2014,1,4)..Date.today
-times = %w[1am, 2am, 3am, 4am, 5am, 6am, 7am, 8am, 9am, 10am, 11am, 12pm,
-          1pm, 2pm, 3pm, 4pm, 5pm, 6pm, 7pm, 8pm, 9pm, 10pm, 11pm, 12am]
+times = %w[1am 2am 3am 4am 5am 6am 7am 8am 9am 10am 11am 12pm
+          1pm 2pm 3pm 4pm 5pm 6pm 7pm 8pm 9pm 10pm 11pm 12am]
 dates.each do |date|
   times.each do |time|
     begin
