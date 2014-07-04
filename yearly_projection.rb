d3af5110b27d8da162ca6391e8882b7c20e8746d
Miles Starkenburg <milesstarkenburg@gmail.com>
2014-07-03 23:54:44 -0700

    Taste Profiles

--- yearly_projection.rb
+++ yearly_projection.rb
@@ -1,8 +1,15 @@
 years = 2001..2014
 years.each do |year|
   start_date = Time.local(year).utc
-  end_date  = Time.local(year+1).utc
+  end_date   = Time.local(year+1).utc
+  
+  query      = { "AirDate"    => { "$gte" => start_date, "$lt" => end_date },
+                 "ArtistName" => { "$ne"  => null }}
 
-  playlist.find({ "AirDate"    => { "$gte" => start_date, "$lt" => end_date },
-                  "ArtistName" => { "$ne"  => null }})
+  playlist.find(query).each do |doc|
+    id = doc["_id"].to_s
+    artist = doc["ArtistName"]
+    track = doc["TrackName"]
+    taste_object = { "item_id" => id, "artist_name" => artist, "song_name" => track }
+  end
 end
